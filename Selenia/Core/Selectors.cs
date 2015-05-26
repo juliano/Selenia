8890169b42501b342caae14c151631e600afc489
Juliano Alves <juliano.alves@lambda3.com.br>
2015-05-26 16:45:50 -0300

    created ByAttribute selector

---
+++ Selenia/Core/Selectors.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium;
+
+namespace Selenia.Core
+{
+    public class Selectors
+    {
+        public static By ByAttribute(string name, string value) =>
+            By.XPath($".//*[@{name}=\"{value}\"]");
+    }
+}
